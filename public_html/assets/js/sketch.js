3eca25ae7a867ebd8532fb9ce4059fd5f69a9857
Jochem van Iterson <jochem.vaniterson@audioware.nl>
2020-03-30 17:16:39 +0200

    More comments

--- public_html/assets/js/sketch.js
+++ public_html/assets/js/sketch.js
@@ -1,18 +1,18 @@
-var socket = io();
+var socket = io(); // start connection with server via socket.io
+const container = window.document.getElementById('container'); // Get container in which p5js will run
+let MOUSEARMED = false; // Used to handle a click event only once
 
-const container = window.document.getElementById('container');
-const colorlist = ["#000000", "#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"];
-const bgcolor = 250;
-const maxLineSegs = 1024;
-
-let linelist = [];
-let MOUSEARMED = false;
-let chosenColor = "#000000";
+const colorlist = ["#000000", "#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"]; // List of usable colors
+let chosenColor = "#000000"; // Chosen color
+const bgcolor = "#f0f0f0";
+const maxLineSegs = 1024; // Maximum amount of line segments for every possible user
+let linelist = []; // Holder for line segments
+let lastCursor = [0,0]; // Last position of cursor (x,y pixels)
 let isDrawing = false;
-let lastCursor = [0,0];
 
 let sketch = function(p) {
   p.setup = function(){
+    // Create canvas with the size of the container and fill with bgcolor
     p.createCanvas(container.offsetWidth, container.offsetHeight);
     p.background(bgcolor);
   }
@@ -22,33 +22,37 @@ let sketch = function(p) {
     drawLineSegments();
     drawColorChooser();
     drawCursor();
-    // Release mouse armed
+    // Release mouse if armed
     if(MOUSEARMED) MOUSEARMED = false;
   };
   p.windowResized = function() {
     p.resizeCanvas(container.offsetWidth, container.offsetWidth);
   }
+
+  // Handle mouse click events. Set 'MOUSEARMED' to true if mouse clicked, and false on mouse release OR end of draw function
   p.mousePressed = function() {
     MOUSEARMED = true;
   }
   p.mouseReleased = function() {
     MOUSEARMED = false;
   }
+
   function drawColorChooser(){
-    // Draw color choosers
     let padding = 10;
     let itmWidth = 50;
     if(p.width <= 500){
+      // If canvasWidth <= 500 px then change view to a smaller layout
       itmWidth = p.width/colorlist.length;
       padding = 0;
     }
-    let xPos = padding;
-    for (let colorValue of colorlist) {
-      let m = (chosenColor == colorValue)?1.4:1; // Multiplier
+
+    let xPos = padding; // Holder for current x position
+    for (let colorValue of colorlist) { // For every value in colorlist
+      let m = (chosenColor == colorValue)?1.4:1; // Multiplie height if chosen color
       p.fill(colorValue);
       p.stroke(150)
       p.rect(xPos, padding, itmWidth, 50*m);
-      if(
+      if( // Handle click for color chooser
         MOUSEARMED &&
         (p.mouseX>=xPos && p.mouseX<=xPos+itmWidth) &&
         (p.mouseY>=padding && p.mouseY<=50*m)
@@ -60,19 +64,25 @@ let sketch = function(p) {
     }
   }
   function drawCursor(){
+    // Only draw cursor if mouse is drawing
     if(p.mouseIsPressed){
       p.fill(chosenColor);
       p.ellipse(p.mouseX, p.mouseY, 20, 20);
     }
   }
   function handleMouseDrawing(){
+    // Check if mouse is or is supposed to be drawing.
     if(p.mouseIsPressed && !isDrawing){
+      // Mouse is pressed, but not drawing. Changing state and set cursor position
       isDrawing = true;
       lastCursor = [p.mouseX, p.mouseY];
     } else if(p.mouseIsPressed && isDrawing){
+      // Mouse is pressed and drawing.
+      // Check if moved distance is larger than 'n' pixels. If true add line segment to list and send to socket.io
       if(p.dist(lastCursor[0], lastCursor[1], p.mouseX, p.mouseY) > 10){
         var w = container.offsetWidth;
         var h = container.offsetWidth;
+        // Convert mouse position to decimal value.
         socket.emit('drawing', {
           color:chosenColor,
           x0:lastCursor[0] / w,
@@ -87,13 +97,16 @@ let sketch = function(p) {
           p.mouseX,
           p.mouseY
         )
+        // Set last cursor position to current
         lastCursor = [p.mouseX, p.mouseY]
       }
     } else {
+      // Else set drawing state false
       isDrawing = false;
     }
   }
   function drawLineSegments(){
+    // Draw all line segments
     for(lineSeg of linelist){
       p.push();
       p.fill(lineSeg.color);
@@ -106,6 +119,7 @@ let sketch = function(p) {
 };
 new p5(sketch, container);
 socket.on('drawing', (data)=>{
+  // On receive of 'drawing' event: add multiply data with screen size and add data to line list
   var w = container.offsetWidth;
   var h = container.offsetWidth;
   addToLineList(
@@ -118,6 +132,7 @@ socket.on('drawing', (data)=>{
 });
 
 function addToLineList(color, x0, y0, x1, y1){
+  // Create line segment object
   let lineSeg = {
     color: color,
     x0: x0,
@@ -125,6 +140,8 @@ function addToLineList(color, x0, y0, x1, y1){
     x1: x1,
     y1: y1
   }
+  // check length of linelist and remove first if too long
   if(linelist.length>maxLineSegs)linelist.shift()
+  // Add segment to list
   linelist.push(lineSeg);
 }
