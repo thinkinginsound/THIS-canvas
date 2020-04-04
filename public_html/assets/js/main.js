dc0198b175ee96dc6c58028d0ec94d24784fd7df
Jochem van Iterson <jochem.vaniterson@audioware.nl>
2020-04-04 13:55:32 +0200

    Implement welcome modal and terms page

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -1,3 +1,8 @@
+import { WelcomeModal } from  "./modals/welcomeModal.js"
 $(function() {
-  
+  if(!checkCookie("termsagreed")){
+    let welcomeModal = new WelcomeModal();
+    console.log("welcomeModal", welcomeModal)
+    welcomeModal.show();
+  } else window.termsagreed = true
 });
