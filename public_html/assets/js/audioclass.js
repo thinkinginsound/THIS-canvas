4075d528ff09f25c93c0e899105724c0718c02d1
Jochem van Iterson <jochem.vaniterson@audioware.nl>
2020-04-14 11:58:53 +0200

    Voorbeeld en comments

--- public_html/assets/js/audioclass.js
+++ public_html/assets/js/audioclass.js
@@ -4,11 +4,33 @@ class AudioClass{
     console.log("AudioClass started")
     this.p = p;
     this.groupid = -1;
+    this.isHerding = false;
+    this.speed = 500;
+    // Nieuwe variabelen. Begin met 'this.'
+
+    // Start audio
+    this.initAudioEngine();
   }
+
+  // Set data vanuit buiten de class
   setGroupID(groupid){
     console.log("set groupID", groupid);
     this.groupid = groupid
   }
+  setIsHerding(isHerding){
+    this.isHerding = isHerding
+  }
+
+  // Nieuwe functies
+
+  // Functie voor audio engine
+  initAudioEngine(){
+    setInterval(()=>{
+      // Uitvoer ding
+      // 'p.' is nu 'this.p.'
+    }, this.speed)
+  }
 }
 
+// Afblijven
 export { AudioClass };
