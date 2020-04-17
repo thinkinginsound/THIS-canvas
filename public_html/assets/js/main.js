f7db8096d1ada9faa9779b79c394b867f7e6d821
Eva Hunting <eva.hunting@thinkinginsound.nl>
2020-04-17 14:28:22 +0200

    05:00 --> 5:00 minutes and inserted pixels per color. TODO: connect to data

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -28,7 +28,7 @@ function startTimer(duration, display) {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
+        minutes = minutes < 10 ?  + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         display.textContent = minutes + ":" + seconds;
