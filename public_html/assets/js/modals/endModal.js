a98bd62e5658f5dac9d5133c0526fceb8b6bd470
Eva Hunting <eva.hunting@thinkinginsound.nl>
2020-04-17 15:16:17 +0200

    inserted enter

--- public_html/assets/js/modals/endModal.js
+++ public_html/assets/js/modals/endModal.js
@@ -32,6 +32,7 @@ export class EndModal extends DefaultModal {
         <button type="button" class="btn btn-primary" id="bleh">5</button> Constantly
         <div id="page2">
         You behaved like a sheep for ...% of the time
+        </br>
         Please fill in the google forms in the link below
         </br> insert link
         </div>
