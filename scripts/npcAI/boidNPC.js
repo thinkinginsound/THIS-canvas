96d5ba429ea5a23d7323134b7ffc2a4139106465
revess <bjmaat@gmail.com>
2020-04-17 11:05:38 +0200

    improvements to the NPC

--- scripts/npcAI/boidNPC.js
+++ scripts/npcAI/boidNPC.js
@@ -4,19 +4,23 @@ class boidNPC{
         this.canvasheight = canvasheight;
         this.x = startX;
         this.y = startY;
+        this.prevX = this.x;
+        this.prevY = this.y;
         this.innerCircle = 1;
         this.outerCircle = 4;
     }
 
     moveSpace(directionX,directionY){
-        if(this.x < this.canvaswidth-1 && this.x > 1){
+        this.prevX = this.x;
+        this.prevY = this.y;
+        if(this.x < this.canvaswidth-1 && this.x >= 1){
             this.x += directionX;
         } else if(this.x >= this.canvaswidth-1){
             this.x += (Math.round(Math.random())-1);
         } else {
             this.x += (Math.round(Math.random()));
         }
-        if(this.y < this.canvasheight-1 && this.y > 1){
+        if(this.y < this.canvasheight-1 && this.y >= 1){
             this.y += directionY;
         } else if(this.y >= this.canvasheight-1){
             this.y += (Math.round(Math.random())-1);
@@ -29,20 +33,30 @@ class boidNPC{
         let percentage = Math.floor(Math.random()*100);
         let directionX = 0;
         let directionY = 0;
-        if(percentage <= 40){
-            directionX = 1
-        } else if(percentage > 40 && percentage <=80){
-            directionX = -1
+        if(percentage <= 60){
+            directionX = this.x-this.prevX;
         } else {
-            directionX = 0
+            percentage = Math.floor(Math.random()*100);
+            if(percentage <= 40){
+                directionX = 1
+            } else if(percentage > 40 && percentage <=80){
+                directionX = -1
+            } else {
+                directionX = 0
+            }
         }
         percentage = Math.floor(Math.random()*100);
-        if(percentage <= 40){
-            directionY = 1
-        } else if(percentage > 40 && percentage <=80){
-            directionY = -1
+        if(percentage <= 60){
+            directionY = this.y-this.prevY;
         } else {
-            directionY = 0
+            percentage = Math.floor(Math.random()*100);
+            if(percentage <= 40){
+                directionY = 1
+            } else if(percentage > 40 && percentage <=80){
+                directionY = -1
+            } else {
+                directionY = 0
+            }
         }
         this.moveSpace(directionX,directionY);
     }
