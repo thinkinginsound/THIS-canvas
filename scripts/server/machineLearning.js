5fd9e1781a8d5a07a09c6203538d8bf4dc221ba2
revess <bjmaat@gmail.com>
2020-04-22 14:45:03 +0200

    created machinelearningClass

--- scripts/server/machineLearning.js
+++ scripts/server/machineLearning.js
@@ -4,7 +4,87 @@ loadModel()
 getAIHerding()
 */
 
+const tf = require("@tensorflow/tfjs-node");
 
+class MLManager{
+    constructor(modelPath,usergroups){
+        this.herdingList = [];
+        this.model = undefined;
+        this.loadModelFile(modelPath);
+        for(let i = 0; i < usergroups; i++){
+            this.heardingList.push([])
+        }
+    }
+
+    async loadModelFile(modelPath){
+        this.model = await tf.loadLayersModel(modelPath);
+      }
+
+    prediction(EDMatrix){
+        if(this.model === undefined)return;
+        EDMatrix.forEach((group,index)=>{
+            let toPredict = tf.cast(tf.tensor2d(group),'int32');
+            let predlabels = this.model.predict(toPredict);
+            let grouplist = []
+            predlabels.arraySync().forEach(row => {
+                let rows = [];
+                row.forEach(value => {
+                    if(value >=0.5){rows.push(1)}
+                    else{rows.push(0)};
+                });
+                grouplist.push(rows);
+            });
+            this.herdingList[index] = grouplist.slice();
+        });
+    }
+
+    getHerdingList(){
+        return this.herdingList;
+    }
+
+    addUsergroup(numberOfGroups){
+        for(let i = 0; i < numberOfGroups; i++){
+            this.herdingList.push([]);
+        }
+    }
+
+    removeUsergroup(numberOfGroups){  //StillBeta
+        for(let i = 0; i < numberOfGroups; i++){
+            this.herdingList.pop();
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  async function demo() {
+    let dead = new MLManager("file://../../data/model/model.json");
+    for(let i = 0; i < 2; i++){
+        let pred = dead.prediction([
+            [
+                [1,1,1,1],
+                [0,0,0,0]
+            ],
+            [
+                [1,1,1,1],
+                [0,0,0,0]
+            ],
+            [
+                [1,1,1,1],
+                [0,0,0,0]
+            ],
+            [
+                [1,1,1,1],
+                [0,0,0,0]
+            ]
+        ]);
+        await sleep(500);
+    }
+}
+  
+demo();
 
 /*
 ~~~OldCode~~~
