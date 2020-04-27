32d25ed245b001a19be395571bc1cf0fa226a687
Eva Hunting <eva.hunting@thinkinginsound.nl>
2020-04-27 14:22:18 +0200

    Welcome Modal bug fixes and sound test. TODO: fix sound test

--- public_html/assets/js/modals/welcomeModal.js
+++ public_html/assets/js/modals/welcomeModal.js
@@ -4,23 +4,62 @@ export class WelcomeModal extends DefaultModal {
   constructor(){
     let options = {
       id:"welcome-modal",
-      title:"Welcome",
-      positiveText:"Agree",
+      title:"The Black Sheep",
+      positiveText:"Next",
       negativeText:"Disagree",
       showHeaderClose:false,
       showFooterClose:false,
-      showFooterNegative:true,
+      showFooterNegative:false,
+      showFooterPositive:false,
     }
+
     super(options);
     this.setBody($(`
+      <audio id="myAudio">
+        <source src="testSound.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>
       <div>
-        <p> Welcome to our app. WELCOME!!!
-        </br>Please turn on your sound.
-        </br> You can use the arrow keys to move and the spacebar to colorize your cube.
-        <img src="/assets/images/movementExample.svg" style="width:100%;height:100%;" </img>
-        </br> We use functional cookies for temporary storage of your impersonal id. If you continue you agree to the <a href="terms.html">terms</a> and indicate that you are over 13 years old.</p>
+        <div id="page1">
+          <p> Welcome. Thank you for your participation!
+          </br>
+          </br><b>Here are some instructions:</b>
+          </br> You can use the arrow keys to move one block at a time. Use the spacebar to draw a cube once the bar on the right side of the screen is filled.
+          </br> Please stay until the end of the game. When the timer hits 0:00 another popup will appear.
+          <img src="/assets/images/movementExample.svg" style="width:100%;height:100%;" class="padding"</img>
+          </br> We use functional cookies for temporary storage of your impersonal id. If you continue you agree to the <a href="terms.html">terms</a> and indicate that you are over 13 years old.</p>
+          <div role="group" aria-label="First group" id="agreeButton">
+            <button type="button">Agree</button>
+          </div>
+          </div>
+        <div id="page2">
+          <p>Click the button to test your audio.</p>
+          <div role="group" aria-label="First group" id="playButton">
+            <button type="button">Play Audio</button>
+          </div>
+          </p>
+        </div>
       </div>
     `));
+
+    //<script>
+    // let sound = document.getElementById("myAudio");
+    //   this.view.find("#playButton").click((event)=>{
+    //   this.sound.play();
+    // })
+
+
+    //</script>
+    // Hide dom element with id 'page2'
+    this.view.find("#page2").hide();
+    this.view.find("#agreeButton").click((event)=>{
+      // Hide dom element with id 'page1'
+      this.view.find("#page1").hide();
+      // Show dom element with id 'page2'
+      this.view.find("#page2").show();
+      this.setShowFooterPositive(true);
+    })
+
     this.setActionPositive((e)=>{
       return true;
     })
