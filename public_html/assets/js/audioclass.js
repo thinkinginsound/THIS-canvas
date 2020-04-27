6ea7c23ec63cb8ad11e067125d2fcd120521dde0
Nika Nuijten <nika.nuijten@thinkinginsound.nl>
2020-04-27 12:40:23 +0200

    Clean up

--- public_html/assets/js/audioclass.js
+++ public_html/assets/js/audioclass.js
@@ -7,26 +7,25 @@ class AudioClass{
     this.p = p;
     this.groupid = -1;
     this.isHerding = false;
-    this.speed = 100;
+    this.speed = 200;
+
+    // chords
     this.chord=[60,64,67];
+    this.bassNote=[48];
     this.grondtoonIndex=0;
     this.tertsIndex=1;
     this.kwintIndex=2;
     this.chordType="major";
-    this.bassNote=[48];
-    this.callBreak=false;
     this.chordBeat=0;
+    //this.callBreak=false; //TODO: use callBreak to initiate breaks
 
+    // Rythm
     this.voorkomkans = 8; //TODO: moet gekoppeld worden aan een variabele
     this.chancement = 0;
     this.rhythmNote = 'C3';
     this.rhythmNote2 = 'G3';
     this.rhythmBeat = 0;
-    // Nieuwe variabelen. Begin met 'this.'
-    this.baseChord = [60, 64, 70];
-    this.currentChord = this.baseChord; // pakt nu frequenties inplaats van midi nootnummers
-    this.prevChord = [-1,-1,-1];
-    // GEBRUIK de lijst this.currentChord om constant te spelen!
+
     // Start audio
     this.initAudioEngine();
     this.randomPercentage = 0;
@@ -72,12 +71,12 @@ riemann(){
   }
   if (this.chordBeat==1){
     let choice = this.p.round(this.p.random(0,100)); // random keuze voor welke noot verandert
-    // console.log(this.chord);
-    // console.log(this.chordType);
-    // console.log("Grondtoon= ", Tone.Frequency(this.chord[this.grondtoonIndex], "midi").toNote());
-    // console.log("Terts= ", Tone.Frequency(this.chord[this.tertsIndex], "midi").toNote());
-    // console.log("Kwint= ", Tone.Frequency(this.chord[this.kwintIndex], "midi").toNote());
-    if (choice < 20){
+    console.log(this.chord);
+    console.log(this.chordType);
+    console.log("Grondtoon= ", Tone.Frequency(this.chord[this.grondtoonIndex], "midi").toNote());
+    console.log("Terts= ", Tone.Frequency(this.chord[this.tertsIndex], "midi").toNote());
+    console.log("Kwint= ", Tone.Frequency(this.chord[this.kwintIndex], "midi").toNote());
+    if (choice < 30){
       // Grondtoonverandering
       if(this.chordType == "major"){
         this.chord[this.grondtoonIndex]-=1;
@@ -85,7 +84,7 @@ riemann(){
           this.chord[this.grondtoonIndex]-=2;
         }
     }
-    if (choice >= 20 && choice < 40){
+    if (choice >= 30 && choice < 60){
       // Tertsverandering
       if(this.chordType == "major"){
         this.chord[this.tertsIndex]-=1;
@@ -93,7 +92,7 @@ riemann(){
           this.chord[this.tertsIndex]+=1;
         }
     }
-    if (choice >= 40 && choice < 60){
+    if (choice >= 60 && choice < 90){
       // Kwintverandering
       if(this.chordType == "major"){
         this.chord[this.kwintIndex]+=2;
@@ -117,17 +116,9 @@ riemann(){
     // Sort numbers from hight to low
     sortedPrev.sort((a, b) => a - b);
     sortedNew.sort((a, b) => a - b);
-    // Dit kan misschien weg? (zorgt dat het niet breder dan 1,5 octaaf wordt)
-    // if(sortedPrev[2] >= sortedPrev[0]+18){
-    //   sortedPrev[2] = sortedPrev[0]+18;
-    // }
-    // if(sortedNew[2] >= sortedNew[0]+18){
-    //   sortedNew[2] = sortedNew[0]+18;
-    // }
     let chordToPlay = [];
     let chordToNotPlay = [];
     let push = true;
-    // Dit kan misschien ook weg? (zorgt dat dubbele noten niet 2x gepusht worden)
     sortedNew.forEach((note,indexNote) => {
       sortedPrev.forEach((prevNote) => {
         if(note == prevNote){
@@ -136,7 +127,7 @@ riemann(){
         }
       });
       if(push){
-        // push noten naar leesbare lijst voor synth (C4 ipv 6 naar)
+        // push noten naar leesbare lijst voor synth (C4 ipv 60 naar)
         chordToNotPlay.push(Tone.Frequency(sortedPrev[indexNote], "midi").toNote());
         chordToPlay.push(Tone.Frequency(sortedNew[indexNote], "midi").toNote());
       } else {
@@ -200,11 +191,8 @@ riemann(){
   // Functie voor audio engine
   initAudioEngine(){
     setInterval(()=>{
-      this.rhythmPlayer();
-      // Uitvoer ding
-      //this.newBaseChord();
+      //this.rhythmPlayer();
       this.riemann();
-      // 'p.' is nu 'this.p.'
     }, this.speed)
   }
 }
